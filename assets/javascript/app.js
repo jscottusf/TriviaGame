fef8ad582583f5f281d56ab1b255b7f2c24868b9
Joel Scott <scotjoe@mysdhc.net>
2020-01-13 15:42:25 -0500

    working on getting question and choices to display on screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    $("#game-window").html('<button type="button" class="btn btn-dark">Begin</button>')
+    $("#game-window").html('<button type="button" class="begin btn btn-dark">Begin</button>')
 };
 $(document).ready(function() {
     //variables
@@ -24,12 +24,38 @@ $(document).ready(function() {
             answer: "a"
         }
     };
+    let questionNumber = 0;
     let showQuestion;
     let count;
 
     console.log(questions[0]);
     console.log(questionFacts[questions[0]].question);
     console.log(questionFacts[questions[0]].answer);
+
+    function initializeGame() {
+        questionNumber = 0;
+    }
+
+    // function displayQuestion() {
+    //     var qDiv = $("<div class='" + questionFacts[questions[questionNumber]].question) + "'>");~
+    //     var aDiv = $("<p>a. ' + questionFacts[questions[questionNumber]].question.a + '</p>"');
+    //     var bDiv = $()
+    //     var cDiv = $()~
+    //     var dDiv = $()
+        
+    //     $("#game-window").append('"<p>b. "' + questionFacts[questions[questionNumber]].question.b) + '"</p>"');
+    //     $("#game-window").append('"<p>c. "' + questionFacts[questions[questionNumber]].question.c) + '"</p>"');
+    //     $("#game-window").append('"<p>d. "' + questionFacts[questions[questionNumber]].question.d) + '"</p>"');~
+    //   }
+
+    // function renderQuestion() {
+    //     let questionDiv = $("<div>"+  + "</div>")
+    // }
+
+    $(".begin").on("click", function() {
+        $(this).remove();
+        displayQuestion();
+    });
 
     
 
