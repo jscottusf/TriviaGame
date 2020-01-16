ca39f249652a4260d0fc16046e8303add9c94789
Joel Scott <scotjoe@mysdhc.net>
2020-01-16 08:49:26 -0500

    game is now able to check if answer is correct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,31 @@
 window.onload = function() {
     var startButton = $('<button type="button" class="begin btn btn-dark">Begin</button>')
-    $("#game-window").append('<button type="button" class="begin btn btn-dark">Begin</button>')
+    $("#game-window").append(startButton);
 };
+
 $(document).ready(function() {
     let questions = [
-        {number: 1, q: "Do you like cats?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer dogs", answer: "yes, but I prefer dogs", image: "./assets/images/js.png"},
-        {number: 2, q: "Do you like dogs?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer cats", answer: "yes", image: "./assets/images/js.png"}
+        {number: 1, q: "Do you like cats?",  "a": "yes", "b": "no", "c": "maybe", "d": "yes, but I prefer dogs", ansChar: "d", answer: "yes, but I prefer dogs", image: "./assets/images/js.png"},
+        {number: 2, q: "Do you like dogs?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer cats", ansChar: "a", answer: "yes", image: "./assets/images/js.png"},
+        {number: 3, q: "Do you like hamsters?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer mice", ansChar: "c", answer: "maybe", image: "./assets/images/js.png"}
     ];
     let questionNumber;
-    let count;
+    let userGuess;
+    let timeRemaing;
+    let timeFeedback;
     let intervalId;
     let feedback;
-
-    console.log(questions[0]);
-    console.log(questions[0].q);
-    console.log(questions[0].answer);
+    let answerKey;
 
     function initializeGame() {
         questionNumber = 0;
-        run();
+        timeRemaing = 10;
+        timeFeedback = 5;
     }
 
     function displayQuestion() {
-        isQuestion = true;
-        count = 30;
+        runT();
+        resetScreen();
         var cardDiv = $('<div class="cardDiv">');
         $("#game-window").append(cardDiv);
         var qDiv = $('<div class="question">' + questions[questionNumber].q + '</div>');
@@ -31,52 +33,97 @@ $(document).ready(function() {
         var bDiv = $('<div class="choice" id="b">b. ' + questions[questionNumber].b + '</div>');
         var cDiv = $('<div class="choice" id="c">c. ' + questions[questionNumber].c + '</div>');
         var dDiv = $('<div class="choice" id="d">d. ' + questions[questionNumber].d + '</div>');
-        var timeDiv = $('<div id="time">Time Remaining: ' + count + ' seconds</div>');
+        answerKey = questions[questionNumber].ansChar;
+        var timeDiv = $('<div id="time">Time Remaining: 10 seconds</div>');
         $(".cardDiv").append(qDiv, aDiv, bDiv, cDiv, dDiv, timeDiv);
+        answerSelected();
+        timeLimit();
     }
 
     function displayFeedback() {
-        isQuestion = false;
-        count = 5;
-        feedback = "Correct! The answer is " + questions[questionNumber].answer;
+        runF();
+        timeRemaing = 10;
+        timeFeedback = 5;
+        resetScreen();
         var cardDiv = $('<div class="cardDiv">');
         $("#game-window").append(cardDiv);
         var feedbackDiv = $('<div>' + feedback + '</div>');
         var imageDiv = $('<img src="' + questions[questionNumber].image + '" width="200px">');
-        var timeDiv = $('<div id="time">Next question in... ' + count + ' seconds</div>');
+        var timeDiv = $('<div id="time">Next question in... 5 seconds</div>');
         $(".cardDiv").append(feedbackDiv, imageDiv, timeDiv);
+        //setTimeout(nextQuestion, 5000);
     }
 
-    function clearBoard() {
+    function nextQuestion() {
+        questionNumber++;
+        timeRemaing = 10;
+        timeFeedback = 5;
+        userGuess = "";
+        displayQuestion();
+    }
+
+    function timeLimit() {
+        checkAnswer();
+    }
+
+    
+
+    function answerSelected() {
         $(".choice").on("click", function() {
-            $(".cardDiv").remove();
+            userGuess = $(this).attr("id");
+            if (userGuess===answerKey) {
+                feedback = "Correct! The answer is " + questions[questionNumber].answer;
+            }
+            else {
+                feedback = "Nope! The answer was " + questions[questionNumber].answer;
+            }
+            resetScreen();
             displayFeedback();
         });
     }
 
-    function run() {
+    function runT() {
         clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-      }
+        intervalId = setInterval(decRemainingTime, 1000);
+    }
 
-    function decrement() {
-        count--;
-        $("#time").html('<div id="time">Time Remaining: ' + count + ' seconds</div>');
-        if (count === 0) {
+    function runF() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decTimeFeedback, 1000);
+    }
+
+    function decRemainingTime() {
+        timeRemaing--;
+        $("#time").html('<div id="time">Time Remaining: ' + timeRemaing + ' seconds</div>');
+        if (timeRemaing === 0) {
+        feedback = "You didn't respond in time...the answer was " + questions[questionNumber].answer;
+        stop();
+            resetScreen();
+            displayFeedback();
+        }
+    }
+
+    function decTimeFeedback() {
+        timeFeedback--
+        $("#time").html('<div id="time">Next question in... ' + timeFeedback + ' seconds</div>');
+        if (timeFeedback === 0) {
             stop();
+            resetScreen();
+            nextQuestion();
         }
     }
 
     function stop() {
         clearInterval(intervalId);
-      }
+    }
+
+    function resetScreen() {
+        $(".cardDiv").remove();
+    };
 
     $(".begin").on("click", function() {
         $(this).remove();
         initializeGame();
         displayQuestion();
-        clearBoard();
     });
-
-    
 });
