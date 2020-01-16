c5449885ffc9fa37cc0656c2f13c9cc405548e80
Joel Scott <jscottusf@gmail.com>
2020-01-15 21:14:53 -0500

    most basic game functionality works, main trouble is getting the question to mark self as wrong at 0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,29 +4,27 @@ window.onload = function() {
 };
 $(document).ready(function() {
     let questions = [
-        {number: 1, q: "Do you like cats?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer dogs", answer: "yes, but I prefer dogs", image: "./assets/images/js.png"},
-        {number: 2, q: "Do you like dogs?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer cats", answer: "yes", image: "./assets/images/js.png"},
-        {number: 3, q: "Do you like hamsters?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer mice", answer: "maybe", image: "./assets/images/js.png"}
+        {number: 1, q: "Do you like cats?",  "a": "yes", "b": "no", "c": "maybe", "d": "yes, but I prefer dogs", ansChar: "d", answer: "yes, but I prefer dogs", image: "./assets/images/js.png"},
+        {number: 2, q: "Do you like dogs?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer cats", ansChar: "a", answer: "yes", image: "./assets/images/js.png"},
+        {number: 3, q: "Do you like hamsters?",  a: "yes", b: "no", c: "maybe", d: "yes, but I prefer mice", ansChar: "c", answer: "maybe", image: "./assets/images/js.png"}
     ];
     let questionNumber;
-    let count;
-    let timeRemaing = 10;
-    let timeFeedback = 5;
+    let userGuess;
+    let timeRemaing;
+    let timeFeedback;
     let intervalId;
     let feedback;
-    var isQuestion;
-
-    console.log(questions[0]);
-    console.log(questions[0].q);
-    console.log(questions[0].answer);
+    //userGuess = undefined;????
 
     function initializeGame() {
         questionNumber = 0;
+        timeRemaing = 10;
+        timeFeedback = 5;
     }
 
     function displayQuestion() {
         runT();
-        isQuestion = true;
+        resetScreen();
         var cardDiv = $('<div class="cardDiv">');
         $("#game-window").append(cardDiv);
         var qDiv = $('<div class="question">' + questions[questionNumber].q + '</div>');
@@ -36,63 +34,99 @@ $(document).ready(function() {
         var dDiv = $('<div class="choice" id="d">d. ' + questions[questionNumber].d + '</div>');
         var timeDiv = $('<div id="time">Time Remaining: 10 seconds</div>');
         $(".cardDiv").append(qDiv, aDiv, bDiv, cDiv, dDiv, timeDiv);
+        answerSelected();
+        timeLimit();
     }
 
     function displayFeedback() {
         runF();
-        isQuestion = false;
-        feedback = "Correct! The answer is " + questions[questionNumber].answer;
+        timeRemaing = 10;
+        timeFeedback = 5;
+        resetScreen();
         var cardDiv = $('<div class="cardDiv">');
         $("#game-window").append(cardDiv);
         var feedbackDiv = $('<div>' + feedback + '</div>');
         var imageDiv = $('<img src="' + questions[questionNumber].image + '" width="200px">');
         var timeDiv = $('<div id="time">Next question in... 5 seconds</div>');
         $(".cardDiv").append(feedbackDiv, imageDiv, timeDiv);
+        setTimeout(nextQuestion, 5000);
+    }
+
+    function nextQuestion() {
+        questionNumber++;
+        timeRemaing = 10;
+        timeFeedback = 5;
+        displayQuestion();
+    }
+
+    function timeLimit() {
+        checkAnswer();
+        //setTimeout(displayFeedback, 10000);
+    }
+
+    function checkAnswer() {
+        userGuess = $(this).attr("id");
+        if (userGuess === questions[questionNumber].ansChar) {
+            feedback = "Correct! The answer is " + questions[questionNumber].answer;
+        }
+        // else if (userGuess = undefined) {
+        //     feedback = "You didn't respond in time...the answer was " + questions[questionNumber].answer;
+        // }
+        else {
+            feedback = "Nope! The answer was " + questions[questionNumber].answer;
+        }
     }
 
     function answerSelected() {
         $(".choice").on("click", function() {
-            $(".cardDiv").remove();
+            resetScreen();
             displayFeedback();
+            checkAnswer();
         });
     }
 
-    function runT() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decRemainingTime, 1000);
-      }
+     function runT() {
+         clearInterval(intervalId);
+         intervalId = setInterval(decRemainingTime, 1000);
+       }
 
-    function runF() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decTimeFeedback, 1000);
-    }
+     function runF() {
+         clearInterval(intervalId);
+         intervalId = setInterval(decTimeFeedback, 1000);
+     }
 
-    function decRemainingTime() {
-        timeRemaing--;
-        $("#time").html('<div id="time">Time Remaining: ' + timeRemaing + ' seconds</div>');
-        if (timeRemaing === 0) {
-            stop();
-        }
-    }
+     function decRemainingTime() {
+         timeRemaing--;
+         $("#time").html('<div id="time">Time Remaining: ' + timeRemaing + ' seconds</div>');
+         if (timeRemaing === 0) {
+             stop();
+         }
+     }
 
-    function decTimeFeedback() {
-        timeFeedback--
-        $("#time").html('<div id="time">Next question in... ' + timeFeedback + ' seconds</div>');
-        if (timeFeedback === 0) {
-            stop();
-        }
-    }
+     function decTimeFeedback() {
+         timeFeedback--
+         $("#time").html('<div id="time">Next question in... ' + timeFeedback + ' seconds</div>');
+         if (timeFeedback === 0) {
+             stop();
+         }
+     }
 
-    function stop() {
-        clearInterval(intervalId);
-      }
+     function stop() {
+         clearInterval(intervalId);
+       }
+
+       function resetScreen() {
+         $(".cardDiv").remove();
+       };
 
     $(".begin").on("click", function() {
         $(this).remove();
         initializeGame();
         displayQuestion();
-        answerSelected();
     });
+
+    // runT();
+    // runF();
 
     
 });
